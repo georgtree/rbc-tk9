d3b3f125e33c506429c659d4fd8be20524e9b45c
Георгий Яшин <georgtree@gmail.com>
2026-08-20 15:48:56 +0100

    docs and build system update

--- docs/doc_gen.tcl
+++ docs/doc_gen.tcl
@@ -28,9 +28,10 @@ set namespaces [list ::TclTk9Upgrade ::rbc ::rbc::GRAPHINST ::rbc::AXIS ::rbc::M
                         ::rbc::LINEELEMENT ::rbc::STRIPELEMENT ::rbc::BARELEMENT ::rbc::PEN ::rbc::MARKER\
                         ::rbc::LEGEND ::rbc::GRID ::rbc::CROSSHAIRS ::rbc::POSTSCRIPT\
                         ::rbc::vector ::rbc::VECINST ::rbc::spline ::rbc::winop ::rbc::WINOPIMAGE]
+set namespacesNroff $namespaces
 
 ruff::document $namespaces -format sphinx -outfile rbc-tk9.rst -outdir [file join $docDir sphinx] {*}$commonSphinx
-ruff::document $namespaces -format nroff -outdir $docDir -outfile rbc.n {*}$commonNroff
+ruff::document $namespacesNroff -format nroff -outdir $docDir -outfile rbc.n {*}$commonNroff
 
 ::fileutil::appendToFile [file join $docDir sphinx conf.py] {html_theme = "classic"
 extensions = [
@@ -54,3 +55,49 @@ def setup(app):
 
 catch {exec sphinx-build -b html [file join $docDir sphinx] [file join $docDir]} errorStr
 puts $errorStr
+
+# nroff pages names processing
+foreach file [glob -directory $docDir *.n] {
+    set old $file
+    set tmp [file join $docDir __temp_rename__.n]
+    set new [file join $docDir [string tolower [file tail $file]]]
+    file rename $old $tmp
+    file rename $tmp $new
+}
+
+set specialPages [list  rbc-tcltk9upgrade]
+
+foreach namespacePath $namespacesNroff {
+    set tails [list]
+    while {$namespacePath ne {}} {
+        set tail [string tolower [namespace tail $namespacePath]]
+        regsub -all {\s+} [string trim $tail] {-} tail
+        set namespacePath [namespace qualifiers $namespacePath]
+        lappend tails $tail
+    }
+    lappend tails [string tolower rbc]
+    set manFileName [join [lreverse $tails] -]
+    if {$manFileName ni $specialPages} {
+        lappend manFilesLinks "${manFileName}(n)"
+    }
+}
+
+set linksString ".SH SEE ALSO
+rbc(n) - package's main page
+.br
+rbc-tcltk9upgrade(n) - list of upgrades comparing to Tcl/Tk8 RBC package
+.br
+.sp 1
+Public commands and classes documentation:
+.br
+[join $manFilesLinks \n.br\n]"
+
+proc addLinks2man {fileContents} {
+    global linksString
+    append fileContents "\n$linksString"
+    return $fileContents
+}
+
+foreach file [glob -directory $docDir *.n] {
+    fileutil::updateInPlace $file addLinks2man
+}
--- tests/all.tcl
+++ tests/all.tcl
@@ -4,9 +4,6 @@
 # tests.  Execute it by invoking "make test" 
 #
 
-# restart using tclsh \
-#exec tclsh "$0" "$@"
-
 package require tcltest 2
 
 package require Tk ;# This is for a Tk Widget; fail early if no Tk!
@@ -14,7 +11,7 @@ package require Tk ;# This is for a Tk Widget; fail early if no Tk!
 # Get RBC
 package require rbc
 
-eval tcltest::configure [lrange $argv 1 end]
+tcltest::configure {*}$argv
 tcltest::configure -testdir [file normalize [file dirname [info script]]]
 
 tcltest::configure -singleproc 1
